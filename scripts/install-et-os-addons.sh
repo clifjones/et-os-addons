6d75b6721fcba28fa517e09fcadff9096bef3944
clif <jonect@gmail.com>
2025-12-04 15:12:14 -0500

    Fix et-predict-app
    Fix et-predict-app by automatically starting necessary API daemons

--- scripts/install-et-os-addons.sh
+++ scripts/install-et-os-addons.sh
@@ -18,6 +18,14 @@ chmod -v 664 ${ET_HOME}/conf/template.d/*.conf
 # Copy misc Skel files
 cp -v -r ${ET_OS_ADDONS_BASE}/overlay/etc/skel/* ${SKEL}/.
 
+# Copy and enable init.d scripts to fix et-predict-app
+et-log "Installing init.d scripts..."
+cp -v ${ET_OS_ADDONS_BASE}/overlay/etc/init.d/et-api /etc/init.d/.
+cp -v ${ET_OS_ADDONS_BASE}/overlay/etc/init.d/et-mbtileserver /etc/init.d/.
+chmod +x /etc/init.d/et-api /etc/init.d/et-mbtileserver
+update-rc.d et-api defaults
+update-rc.d et-mbtileserver defaults
+
 # This should be done by each script
 #cp -v -r ${ET_OS_ADDONS_BASE}/overlay/usr/share/* /usr/share/.
 
